8eafdbbcf83aecefeb47ee4f6e284807d44cdb74
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2024-01-03 12:48:26 +0500

    Change UI color

--- src/widgets/main-layout/ui/index.tsx
+++ src/widgets/main-layout/ui/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import styles from './styles.module.scss';
 import {Navbar} from "../../navbar";
 import {Outlet} from "react-router-dom";
-import {useAppStore} from "../../../shared/model/app-store.ts";
 
 export const MainLayout = () => {
 
-    const navbarButtons = useAppStore(state => state.navbarButtons);
-
     return (
         <div className={styles.wrapper}>
-            <Navbar buttons={navbarButtons}/>
+            <Navbar/>
             <div className={styles.content}>
                 <Outlet/>
             </div>
