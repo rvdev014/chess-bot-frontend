430f0b9030352b3731e84c1ad60fd0cf14dd3c33
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2024-01-13 22:37:36 +0500

    Add socket path if needed

--- src/shared/api/socket.ts
+++ src/shared/api/socket.ts
@@ -1,9 +1,7 @@
 import {io} from 'socket.io-client';
 import {API_URL, SOCKET_PATH} from "../consts.ts";
 
-export const socket = io(API_URL, {
-    path: SOCKET_PATH,
-});
+export const socket = io(API_URL);
 socket.on('connect_error', (error: any) => {
     console.error('connect_error', error)
 })
